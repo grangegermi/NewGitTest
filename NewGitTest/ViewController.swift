09bab7a053e1c71c1843047482f6418bc8ca3801
grangegermi <dasha.voloshina@gmail.com>
2022-08-08 21:54:36 +0300

    secondcommit

--- NewGitTest/ViewController.swift
+++ NewGitTest/ViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ let a = 1
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
